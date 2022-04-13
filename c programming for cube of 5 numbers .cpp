0b715c8ec2b8b3fec3c9df73f28876a90ea10d62
Martirn <102363832+Martirn@users.noreply.github.com>
2022-04-13 11:10:03 +0300

    Add files via upload

---
+++ c programming for cube of 5 numbers .cpp
@@ -0,0 +1,54 @@
+#include <stdio.h>
+int main()
+ {
+ int i,cube;
+printf("Input number of terms : ");
+scanf("%d", &cube );
+ for(i=1;i<=cube;i++)
+ {
+	 printf("Number is : %d and cube of the %d is :%d \n",i,i, (i*i*i));     
+ }
+printf("end of the loop");
+
+
+return 0;
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
